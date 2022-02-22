a04d051a012925c2f77004a25e55411d68bc7f3d
Jonathan Ruiz <jontheruiz4@gmail.com>
2022-02-22 08:28:30 -0600

    idk

---
+++ Random/evens.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main(){
+
+}
+
+
+bool is_even(int a){
+    if(a == 0){
+        return false;
+    }
+    else if(a == 1) {
+        return false;
+    }
+    else if(a == 2) {
+        return true;
+    }
+    else if(a == 3) {
+        return false;
+    }
+    else if(a == 4) {
+        return true;
+    }
+    else if(a == 5) {
+        return false;
+    }
+    else if(a == 6) {
+        return true;
+    }
+    else if(a == 7) {
+        return false;
+    }
+    else if(a == 8) {
+        return true;
+    }
+}
