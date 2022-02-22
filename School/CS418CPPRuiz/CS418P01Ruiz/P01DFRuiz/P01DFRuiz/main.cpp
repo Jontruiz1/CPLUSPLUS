a28fb9560a1ebd5393f5691e34563497416adc5f
Jontruiz1 <61859616+Jontruiz1@users.noreply.github.com>
2022-02-21 18:53:47 -0600

    i have no idea how to do this

--- School/CS418CPPRuiz/CS418P01Ruiz/P01DFRuiz/P01DFRuiz/main.cpp
+++ School/CS418CPPRuiz/CS418P01Ruiz/P01DFRuiz/P01DFRuiz/main.cpp
@@ -25,15 +25,6 @@ int main() {
 	PuzzleState initial;
 	PuzzleState goal;
 	PuzzleState temp;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-
-=======
-=======
->>>>>>> Stashed changes
-	
->>>>>>> Stashed changes
 
 	cout << "Enter number of rows and columns: ";
 	cin >> grid_size;
@@ -57,18 +48,8 @@ int main() {
 	}
 	else {
 		if (find_solution(initial, goal, solution)) {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 			for (auto i : solution) {
-				cout <<  (i).getMoveName() << endl << (i).getState() << endl;
-=======
-			for (auto a : solution) {
-				cout << a.getState() << endl;
->>>>>>> Stashed changes
-=======
-			for (auto a : solution) {
-				cout << a.getState() << endl;
->>>>>>> Stashed changes
+				cout << (i).getMoveName() << endl << (i).getState() << endl;
 			}
 		}
 		else {
@@ -76,138 +57,20 @@ int main() {
 		}
 	}
 }
-list<PuzzleMove> expand(PuzzleState initial) {
-	list<PuzzleMove> neighbors;
-	PuzzleMove s;
-<<<<<<< Updated upstream
 
-<<<<<<< Updated upstream
 bool member_of(PuzzleState curr, list<PuzzleMove> temp) {
 	for (auto a : temp) {
 		if (a.getState() == curr) return true;
 	}
 	return false;
 }
-
-bool find_solution(PuzzleState initial, PuzzleState goal, list<PuzzleMove>& solution) {
+list<PuzzleMove> expand(PuzzleState initial) {
+	list<PuzzleMove> neighbors;
+	PuzzleMove s;
+}
+bool find_solution(PuzzleState initial, PuzzleState goal, list<PuzzleMove>&solution) {
 	list<PuzzleMove> open; // current nodes that we are traversing, like the stack
 	list<PuzzleMove> closed; // nodes no longer traversing, like visited nodes
 	list<PuzzleMove> result;
 	PuzzleMove curr_move(initial, initial, nullMove);
-	open.push_back(curr_move);
-
-
-	while (!open.empty()) {
-		curr_move = open.back();
-		open.pop_back();
-		PuzzleState curr_s = curr_move.getState();
-		result.push_back(curr_move);
-
-		if (curr_s.canMoveDown()) {
-			curr_move = PuzzleMove(curr_s.moveBlankDown(), curr_s, down);
-			open.push_front(curr_move);
-		}
-		if (curr_s.canMoveLeft()) {
-			curr_move = PuzzleMove(curr_s.moveBlankLeft(), curr_s, (enum MoveType)1);
-			open.push_front(curr_move);
-		}
-		if (curr_s.canMoveUp()) {
-			curr_move = PuzzleMove(curr_s.moveBlankUp(), curr_s, up);
-			open.push_front(curr_move);
-		}
-		if (curr_s.canMoveRight()) {
-			curr_move = PuzzleMove(curr_s.moveBlankRight(), curr_s, (enum MoveType)3);
-			open.push_front(curr_move);
-		}
-
-
-=======
-	if (initial.canMoveDown()) {
-		s = PuzzleMove(initial.moveBlankDown(), initial, down);
-		neighbors.push_back(s);
-	}
-	if (initial.canMoveLeft()) {
-		s = PuzzleMove(initial.moveBlankLeft(), initial, (enum MoveType)1);
-		neighbors.push_back(s);
-	}
-	if (initial.canMoveUp()) {
-		s = PuzzleMove(initial.moveBlankUp(), initial, up);
-		neighbors.push_back(s);
-	}
-	if (initial.canMoveRight()) {
-		s = PuzzleMove(initial.moveBlankRight(), initial, (enum MoveType)3);
-		neighbors.push_back(s);
-	}
-	return neighbors;
-
 }
->>>>>>> Stashed changes
-
-=======
-
-	if (initial.canMoveDown()) {
-		s = PuzzleMove(initial.moveBlankDown(), initial, down);
-		neighbors.push_back(s);
-	}
-	if (initial.canMoveLeft()) {
-		s = PuzzleMove(initial.moveBlankLeft(), initial, (enum MoveType)1);
-		neighbors.push_back(s);
-	}
-	if (initial.canMoveUp()) {
-		s = PuzzleMove(initial.moveBlankUp(), initial, up);
-		neighbors.push_back(s);
-	}
-	if (initial.canMoveRight()) {
-		s = PuzzleMove(initial.moveBlankRight(), initial, (enum MoveType)3);
-		neighbors.push_back(s);
-	}
-	return neighbors;
-
-}
-
->>>>>>> Stashed changes
-bool member_of(PuzzleMove curr, list<PuzzleMove> closed) {
-	if (closed.empty()) {
-		return false;
-	}
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-	
-
-	return true;
-}
-
-=======
-	for (auto a : closed) {
-		if (curr.getState() == a.getState()) return true;
-	}
-=======
-	for (auto a : closed) {
-		if (curr.getState() == a.getState()) return true;
-	}
-	return false;
-}
-
-bool find_solution(PuzzleState initial, PuzzleState goal, list<PuzzleMove>& solution) {
-	PuzzleMove curr_move = PuzzleMove(initial, initial, nullMove);
-	
-	for(auto v : expand(curr_move, solution))
-
-	
-
->>>>>>> Stashed changes
-	return false;
-}
-
-bool find_solution(PuzzleState initial, PuzzleState goal, list<PuzzleMove>& solution) {
-	PuzzleMove curr_move = PuzzleMove(initial, initial, nullMove);
-	
-	for(auto v : expand(curr_move, solution))
-
-	
-
-	return false;
-}
->>>>>>> Stashed changes
