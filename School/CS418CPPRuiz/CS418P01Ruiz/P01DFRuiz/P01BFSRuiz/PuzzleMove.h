6f110a55e993c0646bdf7abac4bafc086e6ccbca
Jonathan Ruiz <jontheruiz4@gmail.com>
2022-02-25 20:37:05 -0600

    e

---
+++ School/CS418CPPRuiz/CS418P01Ruiz/P01DFRuiz/P01BFSRuiz/PuzzleMove.h
@@ -0,0 +1,23 @@
+#ifndef _PUZZLEMOVE_
+#define _PUZZLEMOVE_
+
+#include "PuzzleState.h"
+using namespace std;
+
+enum MoveType { down, left, up, right, nullMove };
+
+class PuzzleMove {
+public:
+	PuzzleMove() { }
+	PuzzleMove(PuzzleState s, PuzzleState p, MoveType m) : state(s), parent(p), move(m) { }
+	PuzzleState& getState() { return state; }
+	PuzzleState& getParent() { return parent; }
+	MoveType getMoveName() { return move; }
+
+private:
+	PuzzleState state;
+	PuzzleState parent;
+	MoveType move;
+};
+#endif
+
