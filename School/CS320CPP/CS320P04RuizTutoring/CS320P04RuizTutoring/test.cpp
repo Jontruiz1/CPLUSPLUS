e258bbbd5250f24a772bcc40e776c7bea7601798
Jonathan Ruiz <jontheruiz4@gmail.com>
2022-04-13 14:18:07 -0500

    idk

--- School/CS320CPP/CS320P04RuizTutoring/CS320P04RuizTutoring/test.cpp
+++ School/CS320CPP/CS320P04RuizTutoring/CS320P04RuizTutoring/test.cpp
@@ -12,16 +12,16 @@ private:
 		unsigned int nVertices = g.getNumVertices();
 		bool found_at_least_one_vertex = false;
 		unsigned int costThru_j;
-		unsigned int best_j = -1;
 		unsigned int aCost;
-		bitset<32> min_S;
 
-		if (S.none()) return D[i][0];
-		if (D[i][0] >= 0) return D[i][S.to_ulong()];
+		bitset<32> min_S;
+		if (S.none()) return g.getEdgeCost(i, 0);
+		if (D[i][S.to_ulong()] >= 0) return D[i][S.to_ulong()];
 
 		unsigned int bestCost = INT_MAX;
-		
-		for (unsigned j = 1; j < nVertices; j++) {
+		unsigned int best_j = -1;
+
+		for (int j = 1; j < nVertices; j++) {
 			if (S.test(j)) {
 				bitset<32> S_Temp = S;
 				aCost = D[i][j] + gCost(j, S_Temp.reset(j), g);
@@ -30,38 +30,42 @@ private:
 					bestCost = aCost;
 					best_j = j;
 				}
-				if (found_at_least_one_vertex) {
-					D[best_j][S.to_ulong()] = bestCost;
-					P[i][S.to_ulong()] = best_j;
-				}
 			}
 		}
 
-		
+		if (found_at_least_one_vertex) {
+			D[best_j][S.to_ulong()] = bestCost;
+			P[i][S.to_ulong()] = best_j;
+		}
 
 		return bestCost;
 	}
 public:
 	
-	/*tuple<int, vector<string>>*/unsigned int solve(Graph& g) {
+	unsigned int /*tuple<int, vector<string>>*/ solve(Graph& g) {
 		vector<int> v_temp;
 		vector<unsigned> v_pathTemp;
 		unsigned int nVertices = g.getNumVertices();
 		unsigned int minCost = 0;
-		
+
 		for (unsigned int i = 0; i < nVertices; i++) {
 			v_temp.resize(static_cast<int>(pow(2, nVertices)));
 			v_temp.assign(v_temp.size(), -1);
+			v_pathTemp.resize(static_cast<int>(pow(2, nVertices)));
+			v_pathTemp.assign(v_temp.size(), -1);
+
 			D.push_back(v_temp);
+			P.push_back(v_pathTemp);
 		}
+
 		for (size_t i = 0; i < nVertices; i++) {
 			S.set(i);
 		}
 		minCost = gCost(0, S.reset(0), g);
 
 		return minCost;
-
 	}
+
 };
 
 
@@ -122,9 +126,8 @@ int main() {
 	tester.print();
 	Problem mainP;
 
-
-
-	cout << mainP.solve(tester);
+	minlength = mainP.solve(tester);
+	cout << minlength;
 	//unsigned int bestTourCost = mainP.computeMinTourCost(0, V.reset(0));
 
 }
