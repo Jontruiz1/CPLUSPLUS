f629f7f72ca8e81f791574daeb9119d5e9f27a4b
Jontruiz1 <61859616+Jontruiz1@users.noreply.github.com>
2022-10-11 00:10:25 -0500

    vcb

---
+++ School/CS420CPPRuiz/CS420Ruiz/CS420Lab02Ruiz/driver.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <thread>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void fileToMemoryTransfer(char* fileName, char** data, size_t& numOfBytes) {
+	streampos begin, end;
+	ifstream inFile(fileName, ios::in | ios::binary | ios::ate);
+	if (!inFile)
+	{
+		cerr << "Cannot open " << fileName << endl;
+		inFile.close();
+		exit(1);
+	}
+
+
+	size_t size = inFile.tellg();
+	char* buffer = new  char[size];
+	inFile.seekg(0, ios::beg);
+	inFile.read(buffer, size);
+	inFile.close();
+	*data = buffer;
+	numOfBytes = size;
+}
+
+
+int main() {
+	string filename;
+	cin >> filename;
+	fileToMemoryTransfer(filename);
+
+}
---
+++ Test/CS318P03Ruiz/Source.cpp
@@ -0,0 +1,9 @@
+#include <cmath>
+
+class complex {
+
+};
+
+complex polar(const double r, const double theta) {
+
+}
