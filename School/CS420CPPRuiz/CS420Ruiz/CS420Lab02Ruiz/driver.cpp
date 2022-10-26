c579391e590b758888750c4bec85d918e0b4f16e
Jontruiz1 <61859616+Jontruiz1@users.noreply.github.com>
2022-10-26 09:10:07 -0500

    lab02

--- School/CS420CPPRuiz/CS420Ruiz/CS420Lab02Ruiz/driver.cpp
+++ School/CS420CPPRuiz/CS420Ruiz/CS420Lab02Ruiz/driver.cpp
@@ -67,17 +67,15 @@ void globalThreadHistogram(char* fileName, const unsigned int THREAD_COUNT, vect
 		}
 
 
-		workers.push_back(thread([&gH, &inFile](unsigned start, unsigned end, mutex m) {
-			m.lock();
+		workers.push_back(thread([&m, &gH, &inFile](unsigned start, unsigned end) {
+			lock_guard<mutex> lg(m);
 			inFile.seekg(start, ios::beg);
 			unsigned char curr;
 			for (unsigned j = start; j < end; ++j) {
 				curr = inFile.get();
 				gH[curr]++;
 			}
-
-			m.unlock();
-			}, start, end, m));
+		}, start, end));
 
 		start = end;
 		end = start + bytesPerThread;
@@ -97,6 +95,60 @@ void globalThreadHistogram(char* fileName, const unsigned int THREAD_COUNT, vect
 	}
 }
 
+void localThreadHistogram(char* fileName, const unsigned int THREAD_COUNT, vector<thread>& workers) {
+	long long unsigned h[256];
+	size_t bytes;
+	unsigned bytesPerThread;
+	unsigned remainingBytes;
+	
+	unsigned curr;
+
+	unsigned start = 0;
+	unsigned end;
+
+	mutex m;
+
+	ifstream inFile(fileName, ios::in | ios::binary | ios::ate);
+	fileValidation(inFile, fileName);
+
+	bytes = inFile.tellg();						// get total number of bytes
+	inFile.seekg(0, ios::beg);					// go back to offset 0 (start of file)
+	bytesPerThread = bytes / THREAD_COUNT;
+	remainingBytes = bytes % THREAD_COUNT;	// extra bytes for one thread to process
+	end = bytesPerThread;						// range for first bytesPerThread bytes set
+
+	for (unsigned i = 1; i <= THREAD_COUNT; ++i) {
+		if (i == THREAD_COUNT) {
+			end += remainingBytes;
+		}
+
+
+		workers.push_back(thread([&m, &inFile](unsigned start, unsigned end) {
+			long long unsigned h[256];
+			for (unsigned i = 0; i < 256; ++i) { h[i] = 0; }
+
+			m.lock();
+			inFile.seekg(start, ios::beg);
+			unsigned char curr;
+			for (unsigned j = start; j < end; ++j) {
+				curr = inFile.get();
+				h[curr]++;
+			}
+
+			m.unlock();
+			}, start, end));
+
+		start = end;
+		end = start + bytesPerThread;
+
+	}
+
+
+
+	inFile.close();
+
+
+}
 
 int main(int argc, char** argv) {
 	
@@ -113,7 +165,9 @@ int main(int argc, char** argv) {
 	globalThreadHistogram(fileName, THREAD_COUNT, workers);
 	cout << endl << endl;
 
-
+	workers.clear();
 	cout << "Run with local histograms: " << endl;
+	localThreadHistogram(fileName, THREAD_COUNT, workers);
+	cout << endl;
 
 }
