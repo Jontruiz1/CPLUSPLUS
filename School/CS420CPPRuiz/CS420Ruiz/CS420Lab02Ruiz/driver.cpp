00a5f305f5156b3116f3eaca5ad4a61e6af50902
Jonathan Ruiz <jontheruiz4@gmail.com>
2022-10-29 13:18:41 -0500

    lab02

--- School/CS420CPPRuiz/CS420Ruiz/CS420Lab02Ruiz/driver.cpp
+++ School/CS420CPPRuiz/CS420Ruiz/CS420Lab02Ruiz/driver.cpp
@@ -9,23 +9,28 @@ using namespace std;
 const int MAX = 256;
 long long unsigned gH[MAX];
 
-void sequentialHistogram(char* fileName) {
-	long long unsigned sH[MAX];
-	for (int i = 0; i < 256; ++i) {
-		sH[i] = 0;
-	}
-	unsigned char curr;
-	size_t bytes;
-	mutex m;
-
+void fileToMemoryTransfer(char* fileName, char** data, size_t& numOfBytes) {
+	streampos begin, end;
 	ifstream inFile(fileName, ios::in | ios::binary | ios::ate);
-	bytes = inFile.tellg();						// get total number of bytes
-	char* data = new char[bytes];
-	inFile.seekg(0, ios::beg);					// go back to offset 0 (start of file)
-	inFile.read(data, bytes);
-	inFile.seekg(0, ios::beg);	// go back to offset 0 (start of file)
-
+	if (!inFile)
+	{
+		cerr << "Cannot open " << fileName << endl;
+		inFile.close();
+		exit(1);
+	}
+	size_t size = inFile.tellg();
+	char* buffer = new  char[size];
+	inFile.seekg(0, ios::beg);
+	inFile.read(buffer, size);
 	inFile.close();
+	*data = buffer;
+	numOfBytes = size;
+}
+
+void sequentialHistogram(const char *data, const size_t& bytes) {
+	long long unsigned sH[MAX] = { 0 };
+	unsigned char curr;
+	mutex m;
 
 	for (int i = 0; i < bytes; ++i) {
 		curr = data[i];
@@ -35,68 +40,62 @@ void sequentialHistogram(char* fileName) {
 	for (unsigned i = 0; i < 256; ++i) {
 		cout << sH[i] << ": h(" << i << ")" << endl;
 	}
-
 }
 
-void globalThreadHistogram(char* fileName, const unsigned int THREAD_COUNT, vector<thread>& workers) {
-	size_t bytes;
-	size_t bytesPerThread;
-	unsigned remainingBytes;
+
+void globalHistogram(const char* data, const size_t& bytes, const unsigned int& THREAD_COUNT, vector<thread>& workers ) {
+	unsigned bytesPerThread = bytes / THREAD_COUNT;
+	unsigned extra = bytes % THREAD_COUNT;
 	unsigned start = 0;
-	unsigned end;
+	unsigned end = bytesPerThread;
 	mutex m;
 	
 
-	ifstream inFile(fileName, ios::in | ios::binary | ios::ate);
-
-	bytes = inFile.tellg();						// get total number of bytes
-	char* data = new char[bytes];
-	inFile.seekg(0, ios::beg);					// go back to offset 0 (start of file)
-	inFile.read(data, bytes);
-	bytesPerThread = bytes / THREAD_COUNT;
-	remainingBytes = bytes % THREAD_COUNT;		// extra bytes for one thread to process
-	end = bytesPerThread;						// range for first bytesPerThread bytes set
-
 	for (unsigned i = 1; i <= THREAD_COUNT; ++i) {
 		if (i == THREAD_COUNT) {
-			end += remainingBytes;
+			end += extra;
 		}
-		workers.push_back(thread([&data, &m, &inFile](unsigned start, unsigned end) {
+
+		workers.push_back(thread([&m](const char* data, unsigned start, unsigned end) {
+			unsigned char curr;
 
 			for (unsigned j = start; j < end; ++j) {
-				unsigned char curr = data[j];
 				lock_guard<mutex> lg(m);
+				curr = data[j];
 				gH[curr]++;
 			}
-
-		}, start, end));
-
+		}, data, start, end));
 		start = end;
 		end = start + bytesPerThread;
 
 	}
 
-	// prevents main thread from finishing before threads finish running
-	for_each(workers.begin(), workers.end(), [](thread& t) { t.join(); });
-	inFile.close();
+	for_each(workers.begin(), workers.end(), [](thread& t) {
+		t.join();
+	});
 
-	for (unsigned i = 0; i < MAX; ++i) {
+	for (unsigned i = 0; i < 256; ++i) {
 		cout << gH[i] << ": h(" << i << ")" << endl;
 	}
+
 }
 
 int main(int argc, char** argv) {
 	
 	const unsigned int THREAD_COUNT = thread::hardware_concurrency();		// get core count
-	vector<thread> workers;													// will hold the worker threads that will read the file
 	char* fileName = argv[1];												// gets the file name;
+	char* buffer[256];
+	vector<thread> workers;													// will hold the worker threads that will read the file
+	size_t bytes;
+
+	fileToMemoryTransfer(fileName, buffer, bytes);
 
 	cout << "Sequential Histogram: " << endl;								// sequential reading of the file into histogram
-	sequentialHistogram(fileName);
+	sequentialHistogram(*buffer, bytes);
 	cout << endl;
 	
 	cout << "Run with one global histogram: " << endl;						// thread processing of the file with a global histogram
-	globalThreadHistogram(fileName, THREAD_COUNT, workers);
+	globalHistogram(*buffer, bytes, THREAD_COUNT, workers);
 	cout << endl;
 
 	workers.clear();
