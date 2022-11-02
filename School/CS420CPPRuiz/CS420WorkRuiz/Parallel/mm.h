8b8cb1e8be32698cc09e0858358192174563ca32
Jonathan Ruiz <jontheruiz4@gmail.com>
2022-11-02 14:13:53 -0500

    moving directory stuffs

--- School/CS420CPPRuiz/CS420WorkRuiz/Parallel/mm.h
+++
@@ -1,7 +0,0 @@
-/* mm.h */
-
-//
-// Matrix Multiplication header file
-//
-
-double** MatrixMultiply(double** const A, double** const B, int N, int numthreads);
--- School/CS420CPPRuiz/CS420WorkRuiz/Sequential/mm.h
+++
@@ -1,8 +0,0 @@
-/* mm.h */
-
-//
-// Matrix Multiplication header file
-//
-
-double** MatrixMultiply(double** const A, double** const B, int N);
-double** NaiveMatrixMultiply(double** const A, double** const B, int N);
