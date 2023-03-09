7892dce189d02aad1d51f7b4f42bacb38f3821a9
Jonathan Ruiz <jontheruiz4@gmail.com>
2023-03-08 21:01:04 -0600

    removes 320 files

--- Test/BlankTutoring/Source.cpp
+++ Test/BlankTutoring/Source.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+class Complex {
+private:
+	double real;
+	double im;
+public:
+	void operator+= (Complex& rhs) {
+		this->real += rhs.im;
+		this->im += rhs.im;
+	
+	}
+
+	Complex() {
+		this->real = 1;
+		this->im = 1;
+	}
+
+	Complex(double a, double b) {
+		this->real = a;
+		this->im = b;
+	}
+};
 
 int main() {
-	do {
+	int x = 5;
+	int y = 2;
+	int tester = x += y;
+	cout << tester;
 
-		std::cout << "Num1";
-		cout << "num2";
 
-		while() {
-			// print the error message
-			cout << "Num2";
-		} 
 
-	} while ();
+	Complex first(5, 4);
+	Complex second(3, 2);
 }
