79fe7464ed545d80011e474b6a46cf147319ba30
Jonathan Ruiz <jontheruiz4@gmail.com>
2023-03-15 15:30:31 -0500

    Removes a tutoring file

--- Test/BlankTutoring/Source.cpp
+++ Test/BlankTutoring/Source.cpp
@@ -4,28 +4,6 @@
 
 using namespace std;
 
-class Complex {
-private:
-	double real;
-	double im;
-public:
-	void operator+= (Complex& rhs) {
-		this->real += rhs.im;
-		this->im += rhs.im;
-	
-	}
-
-	Complex() {
-		this->real = 1;
-		this->im = 1;
-	}
-
-	Complex(double a, double b) {
-		this->real = a;
-		this->im = b;
-	}
-};
-
 int main() {
 	int x = 5;
 	int y = 2;
@@ -34,6 +12,11 @@ int main() {
 
 
 
-	Complex first(5, 4);
-	Complex second(3, 2);
+
+
+}
+
+
+char getMenuChoice() {
+
 }
