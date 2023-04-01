d9cac8ac1feef786576edbc95bb5071e43ccb500
Jonathan Ruiz <jontheruiz4@gmail.com>
2023-04-01 02:56:37 -0500

    Update Prob704BinarySearch.cpp

--- Leetcode/Prob704BinarySearch.cpp
+++ Leetcode/Prob704BinarySearch.cpp
@@ -3,19 +3,25 @@
 
 using namespace std;
 
-int bSearch(vector<int>& nums, int target) {
+// binary search for leetcode
+int bSearch(const vector<int>& nums, int target) {
     int low = 0, high = nums.size()-1;
+
+    // early exit if there's only one or zero elements
     if(high == 0) return (nums[0] == target ? 0 : -1);
-    int mid = (high + low) /2;
+    else if(high == -1) return -1;
 
     while(low < high){
-        if(target == nums[mid]) return mid;
+        // update mid based on current highs and lows
+        int mid = (high + low) /2;
 
-        if(target > nums[mid]) low = mid+1;
-        else high = mid -1;
-        mid = (high + low) /2;
+        // either found the element or update low/high
+        if(target == nums[mid]) return mid;
+        if(target>nums[mid]) low = mid+1;
+        else high = mid- 1;
     }
 
+    // broke out of loop, either it found the element or it's not in the array
     return (nums[low] == target ? low : -1);
 }
 int main(){
@@ -26,6 +32,4 @@ int main(){
     vector<int> second{2, 5};
     int stest = 0;
     cout << bSearch(second, stest);
-    
-
 }
